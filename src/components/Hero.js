196df5cd794a3804cb8fbb6e6e3164af70d9ebbd
Hiken <49207782+Hikennn@users.noreply.github.com>
2022-03-03 10:11:53 +0100

    add navbar with responsive

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+    
+    return (
+        <div className="hero">
+            <div className="navbar">
+                <ul>
+                    <li><a href="#">Qui sommes-nous ?</a></li>
+                    <li><a href="#">Gagnants</a></li>
+                    <li><a href="#">Calendrier</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+
+}
+
+export default Hero;

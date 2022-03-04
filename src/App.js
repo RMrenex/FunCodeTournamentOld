6263c0caf9379f37edc7c40fff0411531356ea27
Hiken <49207782+Hikennn@users.noreply.github.com>
2022-03-04 04:26:27 +0100

    add podium section with responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import About from './components/About';
+import Podium from './components/Podium';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <About />
+      <Podium />
     </div>
   );
 }

79c0c057ff1840ce7efbc08a35298e15b5bed7cf
Hiken <49207782+Hikennn@users.noreply.github.com>
2022-03-06 06:24:24 +0100

    add link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer__social">
                 <ul>
-                    <li><a href="#">DISCORD</a></li>
-                    <li><a href="#">YOUTUBE</a></li>
-                    <li><a href="#">INSTAGRAM</a></li>
+                    <li><a href="https://discord.gg/UpavTYdhyC" target="_blank" rel="noreferrer">DISCORD</a></li>
+                    <li><a href="https://www.youtube.com/channel/UCHWPs-vJg9uGkyIrixR-yCA" target="_blank" rel="noreferrer">YOUTUBE</a></li>
+                    <li><a href="https://www.instagram.com/funcode_tournament/" target="_blank" rel="noreferrer">INSTAGRAM</a></li>
                 </ul>
             </div>
             <div className="footer__copyright"><p>FunCode Tournament - Copyright ©</p></div>

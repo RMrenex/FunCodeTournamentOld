a77367492a0e52af41c6b2d699dfe1206bf6a87b
Hiken <49207782+Hikennn@users.noreply.github.com>
2022-03-06 06:03:52 +0100

    add link into simply navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
         <div className="navbar simple-navbar">
             <ul>
-                <li><a href="">Accueil</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Dashboard</a></li>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/dashboard">Dashboard</a></li>
             </ul>
         </div>
     );
